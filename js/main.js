ce0d5101d067cd821fb9ecac683b73b32b166840
Toju Mikie <40072570+tojumikie@users.noreply.github.com>
2022-09-09 09:12:09 -0400

    added more to the main page

--- js/main.js
+++ js/main.js
@@ -11,4 +11,42 @@ $(document).ready(function(){
     });
 
     $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+      });
+
+    // OwlCarousel
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            544: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky nav menu
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if($('.header_area').length) {
+            $(window).scroll(function() {
+                let scroll = $(window).scrollTop();
+                if(scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                }
+                else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+    navbarFixed();
 });
